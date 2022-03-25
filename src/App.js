da96506aaf836924b6874fa3bcf0cc036fb3b719
Paul Grin <pashkagrin@gmail.com>
2022-03-25 10:31:41 +0300

    Свойства компонентов

--- src/App.js
+++ src/App.js
@@ -2,15 +2,33 @@ import React, { StrictMode } from "react";
 import "./App.css";
 // Компонент не может изменяться
 
-function WhoAmI({ name, surname, link }) {
-  return (
-    <div>
-      <h1>
-        My name is {name}, surname is {surname}
-      </h1>
-      <a href={link}>my vk</a>
-    </div>
-  );
+class WhoAmI extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      //задание состояния объектом state
+      years: 25,
+    };
+  }
+
+  nextYear = () => {
+    this.setState((state) => ({
+      years: state.years + 1,
+    }));
+  };
+
+  render() {
+    const { name, surname, link } = this.props;
+    return (
+      <div>
+        <h1>
+          My name is {name}, surname is {surname}, age - {this.state.years}
+        </h1>
+        <button onClick={this.nextYear}>+++</button>
+        <a href={link}>my vk</a>
+      </div>
+    );
+  }
 }
 
 function App() {
