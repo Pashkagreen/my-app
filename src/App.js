14aff980e00d964126b9aa3f47d0985eb9198e51
Paul Grin <pashkagrin@gmail.com>
2022-03-25 11:04:04 +0300

    События

--- src/App.js
+++ src/App.js
@@ -8,6 +8,7 @@ class WhoAmI extends React.Component {
     this.state = {
       //задание состояния объектом state
       years: 25,
+      position: "",
     };
   }
 
@@ -17,15 +18,32 @@ class WhoAmI extends React.Component {
     }));
   };
 
+  commitInputChanges = (e, color) => {
+    console.log(color);
+    this.setState({
+      position: e.target.value,
+    });
+  };
+
   render() {
     const { name, surname, link } = this.props;
+    const { position, years } = this.state;
+
     return (
       <div>
         <h1>
-          My name is {name}, surname is {surname}, age - {this.state.years}
+          My name is {name}, surname is {surname}, age - {years}, position -{" "}
+          {position}
         </h1>
         <button onClick={this.nextYear}>+++</button>
         <a href={link}>my vk</a>
+        <form>
+          <span>Введите должность:</span>
+          <input
+            type="text"
+            onChange={(e) => this.commitInputChanges(e, "some color")}
+          />
+        </form>
       </div>
     );
   }
